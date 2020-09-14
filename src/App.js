8f138ff5248509723d33456f4cabcb417890ec40
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-14 07:35:22 +0500

    - installed react-rputer@6 - added Checkout component (basic layout complete of right side)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Checkout from './components/Checkout';
 
 function App() {
     return (
-        <div>
-            <Header />
-            <Home />
-        </div>
+        <Router>
+            <div className='app'>
+                <Header />
+
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/checkout' element={<Checkout />} />
+                </Routes>
+
+            </div>
+        </Router>
     );
 }
 
---
+++ src/components/Checkout.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import '../css/Checkout.css';
+
+const Checkout = () => {
+    return (
+        <div className='checkout'>
+            <div className='checkout__left'>
+                <img 
+                    className='checkout__ad'
+                    src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423592668_.jpg'
+                    alt=''
+                />
+                <div>
+                    <h2 className='checkout__title'>
+                        Shopping Basket
+                    </h2>
+                </div>
+            </div>
+
+            <div className='checkout__right'>
+                <h2>The subtotal will go here</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Checkout;
