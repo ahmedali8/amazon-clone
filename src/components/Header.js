20426fb635f095b9d1e490529b2666b5c1d8b124
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-14 07:41:20 +0500

    added Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,18 @@ import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 import '../css/Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className='header'>
-            <img 
-                className='header__logo'
-                src='images/amazon_logo.png'
-                alt=''
-            />        
+            <Link to="/">
+                <img 
+                    className='header__logo'
+                    src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+                    alt=''
+                />        
+            </Link>
             <div className='header__search'>
                 <input 
                     className='header__searchInput'
@@ -46,12 +49,14 @@ function Header() {
                         Prime
                     </span>
                 </div>
-                <div className='header__optionBasket'>
-                    <ShoppingBasketIcon />
-                    <span className='header__optionLineTwo header__basketCount'>
-                        0
-                    </span>
-                </div>
+                <Link to="/checkout">
+                    <div className='header__optionBasket'>
+                        <ShoppingBasketIcon />
+                        <span className='header__optionLineTwo header__basketCount'>
+                            0
+                        </span>
+                    </div>
+                </Link>
             </div>
         </div>
     );
