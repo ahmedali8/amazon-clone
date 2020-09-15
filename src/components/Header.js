82b6f2df61f1f561d8de01e3a0d14dede65e4086
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-15 09:30:38 +0500

    added the handle authentication to sign out when clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,40 +3,47 @@ import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from 'react-router-dom';
 import { useStateValue } from '../context/StateProvider';
+import { auth } from '../firebase/firebaseConfig';
 
 import '../css/Header.css';
 
 
 function Header() {
 
-    const [{ basket }] = useStateValue();
+    const [{ basket, user }] = useStateValue();
+
+    const handleAuthentication = () => {
+        if (user) {
+            auth.signOut();
+        }
+    }
 
     return (
         <div className='header'>
             <Link to="/">
-                <img 
+                <img
                     className='header__logo'
                     src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
                     alt=''
-                />        
+                />
             </Link>
             <div className='header__search'>
-                <input 
+                <input
                     className='header__searchInput'
                     type='text'
                 />
-                <SearchIcon 
+                <SearchIcon
                     className='header__searchIcon'
                 />
             </div>
             <div className='header__nav'>
-                <Link to="/login">
-                    <div className='header__option'>
+                <Link to={!user && '/login'}>
+                    <div onClick={handleAuthentication} className='header__option'>
                         <span className='header__optionLineOne'>
                             Hello Guest
                         </span>
                         <span className='header__optionLineTwo'>
-                            Sign In
+                            {user ? 'Sign Out' : 'Sign In'}
                         </span>
                     </div>
                 </Link>
