1b006b1f598ec5df958664e65898543c8310f574
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-16 13:35:19 +0500

    - added CardElement - added useStripe and useElements - added priceContainer to contain the price, button - added a feature to show errors as the customer types the card - disable the button if the payment is processing, disabled or succeeded - added parameters to state - added handleSubmit and handleChange fuctions

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStateValue } from '../context/StateProvider';
 import CheckoutProduct from './CheckoutProduct';
 import { Link } from 'react-router-dom';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from '../context/reducer';
 
 import '../css/Payment.css';
 
 const Payment = () => {
 
     const [{ basket, user }] = useStateValue();
+
+    const [processing, setProcessing] = useState("")
+    const [disabled, setDisabled] = useState(true);
+    const [succeeded, setSucceeded] = useState(false)
+    const [error, setError] = useState(null);
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        setProcessing(true);
+        
+        // stripe stuff
+
+    }
+
+    const handleChange = (event) => {
+        // listens for changes in Card Element
+        // and display any errors as the customer types their card details
+
+        setDisabled(event.empty);
+        setError(event.error ? event.error.messsage : "");
+    }
 
     return (
         <div className='payment'>
@@ -57,6 +85,29 @@ const Payment = () => {
                     </div>
                     <div className='payment__details'>
                         {/* Stripe code goes here */}
+
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange} />
+
+                            <div className='payment__priceContainer'>
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                        <h3>Order Total: {value}</h3>
+                                    )}
+                                    decimalScale={2}
+                                    value={getBasketTotal(basket)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"$"}
+                                />
+                                <button disabled={processing || disabled || succeeded}>
+                                    <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                                </button>
+                            </div>
+
+                            {/* Errors */}
+                                {error && <div>{error}</div>}  {/* short circuit evaluation */}
+                        </form>
                     </div>
                 </div>
 
