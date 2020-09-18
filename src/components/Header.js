12df3e7386577f8df58bdcbd4daa78c999302b84
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-18 12:33:20 +0500

    fixed Link issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ function Header() {
                 />
             </div>
             <div className='header__nav'>
-                <Link to={!user && '/login'}>
+                <Link to={user ? "/" : '/login'}>
                     <div onClick={handleAuthentication} className='header__option'>
                         <span className='header__optionLineOne'>
                             {user ? `Hello, ${user.email}` : 'Hello, Guest'}
