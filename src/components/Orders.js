e181563f412438f03b7d77c7e74b538cbfa3a84b
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-18 11:35:57 +0500

    added Orders component

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useStateValue } from '../context/StateProvider';
+
 
 import '../css/Orders.css';
+import { db } from '../firebase/firebaseConfig';
+import Order from './Order';
 
 const Orders = () => {
+
+    const [{ user }] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+
+    useEffect(() => {
+        if(user) {
+            db
+            .collection('users')
+            .doc(user?.uid)
+            .collection('orders')
+            .orderBy('created', 'desc')
+            .onSnapshot(snapshot => (
+                setOrders(snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    data: doc.data()
+                })))
+            ))
+
+        } else {
+            setOrders([])
+
+        }
+
+    }, [user]);
+
+
     return (
-        <div>
-            <h1>Orders Page</h1>
+        <div className='orders'>
+            <h1>Your Orders</h1>
+
+            <div className='orders__order'>
+                {orders?.map(order => (
+                    <Order key={order.id} order={order} />
+                ))}
+            </div>
         </div>
     );
 };
