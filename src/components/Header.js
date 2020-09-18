d41e376cedaabdbebb50854fe12301823aa15d0f
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-18 11:35:26 +0500

    added link to Orders component

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,14 +47,16 @@ function Header() {
                         </span>
                     </div>
                 </Link>
-                <div className='header__option'>
-                    <span className='header__optionLineOne'>
-                        Returns
-                    </span>
-                    <span className='header__optionLineTwo'>
-                        & Orders
-                    </span>
-                </div>
+                <Link to="/orders">
+                    <div className='header__option'>
+                        <span className='header__optionLineOne'>
+                            Returns
+                        </span>
+                        <span className='header__optionLineTwo'>
+                            & Orders
+                        </span>
+                    </div>
+                </Link>
                 <div className='header__option'>
                     <span className='header__optionLineOne'>
                         Your
